b6ccbf6946588574bfce99b8f1aeca11eb0f7e94
fathxn <fathan0x1@gmail.com>
2023-11-26 21:22:14 +0700

    add new domain: user

---
+++ internal/model/domain/user.go
@@ -0,0 +1,11 @@
+package domain
+
+import "time"
+
+type User struct {
+	Id           string    `gorm:"primaryKey" db:"id"`
+	Username     string    `gorm:"type:varchar(255);unique;not null" db:"name"`
+	Email        string    `gorm:"type:varchar(255);unique;not null" db:"email"`
+	PasswordHash string    `gorm:"varchar(255);not null" db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
+}
