2f5d742084adf5596cdb4df777c0e8e97fa3aaa2
fathxn <fathan0x1@gmail.com>
2024-07-16 20:34:49 +0700

    fix(errors): move err msg to dto/errors.go

--- internal/model/dto/response.go
+++ internal/model/dto/response.go
@@ -1,14 +1,5 @@
 package dto
 
-const (
-	MsgOk                  = "OK"
-	MsgInternalServerError = "Internal Server Error"
-	MsgBadRequest          = "Bad Request"
-	MsgNotFound            = "Not Found"
-	MsgCreated             = "Created"
-	MsgUnauthorized        = "Incorrect email or password"
-)
-
 type APIResponse struct {
 	Status  int         `json:"status"`
 	Message string      `json:"message"`
