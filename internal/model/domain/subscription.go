351117a562cd8a8ec21dae8189ff45f4321cbef2
fathxn <fathan0x1@gmail.com>
2024-07-17 09:13:32 +0700

    feat(subscription): add new UserSubscription domain

--- internal/model/domain/subscription.go
+++ internal/model/domain/subscription.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Subscription struct {
 	Id        int       `db:"id"`
@@ -11,6 +14,19 @@ type Subscription struct {
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
-type SubscriptionRepository interface{}
+type UserSubscription struct {
+	Id             int       `db:"id"`
+	UserId         string    `db:"user_id"`
+	SubscriptionId int       `db:"subscription_id"`
+	StartDate      time.Time `db:"start_date"`
+	EndDate        time.Time `db:"end_date"`
+	IsActive       int       `db:"is_active"`
+	CreatedAt      time.Time `db:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at"`
+}
+
+type SubscriptionRepository interface {
+	Create(ctx context.Context, subscription *Subscription) error
+}
 
 type SubscriptionService interface{}
