870b2e7b9603b8409f655613f79f8e1a62aa51f1
fathxn <fathan0x1@gmail.com>
2024-07-18 21:04:04 +0700

    feat(user subscription): completed user subscription repository

--- internal/repository/postgres/url_repository.go
+++ internal/repository/postgres/url_repository.go
@@ -15,6 +15,31 @@ func NewURLRepository(db *sqlx.DB) domain.URLRepository {
 	return &urlRepository{db: db}
 }
 
+// Insert implements domain.URLRepository.
+func (r *urlRepository) Insert(ctx context.Context, url *domain.URL) error {
+	query := `
+		INSERT INTO url (user_id, long_url, short_code)
+		VALUES ($1, $2, $3)
+	`
+
+	return r.db.QueryRowxContext(ctx, query, url.UserId, url.LongURL, url.ShortCode).Scan()
+}
+
+// FindByShortCode implements domain.URLRepository.
+func (r *urlRepository) FindByShortCode(ctx context.Context, shortCode string) (*domain.URL, error) {
+	panic("unimplemented")
+}
+
+// FindById implements domain.URLRepository.
+func (r *urlRepository) FindById(ctx context.Context, id int) (*domain.URL, error) {
+	panic("unimplemented")
+}
+
+// FindByUserId implements domain.URLRepository.
+func (r *urlRepository) FindByUserId(ctx context.Context, userId string) ([]domain.URL, error) {
+	panic("unimplemented")
+}
+
 // Delete implements domain.URLRepository.
 func (r *urlRepository) Delete(ctx context.Context, id int) error {
 	execQuery := "DELETE FROM urls WHERE id = $1;"
@@ -24,24 +49,4 @@ func (r *urlRepository) Delete(ctx context.Context, id int) error {
 	}
 
 	return nil
-}
-
-// FindById implements domain.URLRepository.
-func (r *urlRepository) FindById(ctx context.Context, id int) (*domain.URL, error) {
-	panic("unimplemented")
-}
-
-// FindByShortCode implements domain.URLRepository.
-func (r *urlRepository) FindByShortCode(ctx context.Context, shortCode string) (*domain.URL, error) {
-	panic("unimplemented")
-}
-
-// FindByUserId implements domain.URLRepository.
-func (r *urlRepository) FindByUserId(ctx context.Context, userId string) ([]domain.URL, error) {
-	panic("unimplemented")
-}
-
-// Insert implements domain.URLRepository.
-func (r *urlRepository) Insert(ctx context.Context, url *domain.URL) error {
-	panic("unimplemented")
 }
--- internal/repository/postgres/user_subscription_repository.go
+++ internal/repository/postgres/user_subscription_repository.go
@@ -20,7 +20,7 @@ func (r *userSubscriptionRepository) Create(ctx context.Context, userSubscriptio
 	query := `
 		INSERT INTO	user_subscriptions (user_id, subscription_id, start_date, end_date, is_active)
 		VALUES ($1, $2, $3, $4, $5)
-		RETURNING id, created_at, updated_at
+		RETURNING id, created_at, updated_at;
 	`
 
 	return r.db.QueryRowxContext(ctx, query,
@@ -32,22 +32,62 @@ func (r *userSubscriptionRepository) Create(ctx context.Context, userSubscriptio
 	).Scan(&userSubscription.Id, &userSubscription.CreatedAt, &userSubscription.UpdatedAt)
 }
 
-// Delete implements domain.UserSubscriptionRepository.
-func (r *userSubscriptionRepository) Delete(ctx context.Context, id int) error {
-	panic("unimplemented")
-}
-
 // GetById implements domain.UserSubscriptionRepository.
 func (r *userSubscriptionRepository) GetById(ctx context.Context, id int) (*domain.UserSubscription, error) {
-	panic("unimplemented")
+	var userSubscription domain.UserSubscription
+	query := `
+		SELECT id, user_id, subscription_id, start_date, end_date, is_active, created_at, updated_at
+		WHERE id = $1
+		LIMIT 1;
+	`
+	err := r.db.GetContext(ctx, &userSubscription, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return &userSubscription, nil
 }
 
 // GetByUserId implements domain.UserSubscriptionRepository.
 func (r *userSubscriptionRepository) GetByUserId(ctx context.Context, userId string) ([]*domain.UserSubscription, error) {
-	panic("unimplemented")
+	var userSubscriptions []*domain.UserSubscription
+	query := `
+		SELECT id, user_id, subscription_id, start_date, end_date, is_active, created_at, updated_at
+		WHERE user_id = $1
+		LIMIT 1;
+	`
+	err := r.db.SelectContext(ctx, &userSubscriptions, query, userId)
+	if err != nil {
+		return nil, err
+	}
+	return userSubscriptions, nil
 }
 
 // Update implements domain.UserSubscriptionRepository.
 func (r *userSubscriptionRepository) Update(ctx context.Context, userSubscription *domain.UserSubscription) error {
-	panic("unimplemented")
+	query := `
+		UPDATE user_subscriptions
+		SET user_id = $1, subscription_id = $2, start_date = $3, end_date = $4, is_active = $5, updated_at = NOW()
+		WHERE id = $6
+		RETURNING updated_at
+	`
+	return r.db.QueryRowxContext(
+		ctx,
+		query,
+		userSubscription.UserId,
+		userSubscription.SubscriptionId,
+		userSubscription.StartDate,
+		userSubscription.EndDate,
+		userSubscription.IsActive,
+		userSubscription.Id,
+	).Scan(&userSubscription.UpdatedAt)
+}
+
+// Delete implements domain.UserSubscriptionRepository.
+func (r *userSubscriptionRepository) Delete(ctx context.Context, id int) error {
+	query := `DELETE FROM user_subscriptions WHERE id = $1;`
+	_, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
